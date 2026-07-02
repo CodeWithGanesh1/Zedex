dc31e2f64c39d01f7467e7c316cbb00797a171f3
CodeWithGanesh1 <gk9605873@gmail.com>
2026-07-02 18:10:04 +0530

    updating

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://zedex-nine.vercel.app"],
+  origin: ["https://zedex-umber.vercel.app"],
   credentials: true,
 }));
 
