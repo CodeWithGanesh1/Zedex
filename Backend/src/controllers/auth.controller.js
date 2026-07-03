8d9ba8c17e79e2a2b012149c3b7104f1f668906a
CodeWithGanesh1 <gk9605873@gmail.com>
2026-07-03 14:03:34 +0530

    fix: updated google redirect uris and cart api base url

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -104,7 +104,7 @@ export const googleCallback = async (req, res) => {
 
     res.cookie("token", token)
 
-    res.redirect("http://localhost:5173/")
+    res.redirect("https://zedex-mu.vercel.app/")
 }
 
 export const getMe = async (req, res) => {
