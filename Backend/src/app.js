5d5b0432301675c9dbf1ddae774f669215325d5c
CodeWithGanesh1 <gk9605873@gmail.com>
2026-07-06 10:25:38 +0530

    Deploying to Render

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        'http://localhost:5173',                 
+                         
         'https://zedex-mu.vercel.app',           
         'https://zedex-roan.vercel.app',         
         'https://zedex-git-main-ganeshs-projects-761b96d7.vercel.app' 
