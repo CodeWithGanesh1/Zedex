fde7675c6381bead6a60d87749a02177dbc08740
CodeWithGanesh1 <gk9605873@gmail.com>
2026-07-23 16:53:52 +0530

    fix backend url

--- Frontend/src/features/cart/service/cart.api.js
+++ Frontend/src/features/cart/service/cart.api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-const BACKEND_URL = import.meta.env.PROD 
-    ? "https://zedex-2.onrender.com" 
-    : "";
+
+
+    const BACKEND_URL = "";
 
 const authApiInstance = axios.create({
     baseURL: `${BACKEND_URL}/api/auth/`,
