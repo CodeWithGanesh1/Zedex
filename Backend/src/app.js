78ed781daa78a7cd7472db3e9a5297d026a4d94f
CodeWithGanesh1 <gk9605873@gmail.com>
2026-07-23 16:29:57 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: "https://zedex-roan.vercel.app",
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
+    methods: [ "GET", "POST", "PUT","PATCH", "DELETE" ],
     credentials: true
 }))
 
